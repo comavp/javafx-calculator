6845ed30540ff8266901881eaad127086ffc1e50
comavp <comavp@yandex.ru>
2023-04-19 21:34:11 +0400

    Improve calculator's UI
    * Stage title and icon were added
    * More beautiful colors and readable text size were added
    * Text label was pinned to the top of the stage

--- src/main/java/ru/comavp/App.java
+++ src/main/java/ru/comavp/App.java
@@ -4,23 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        InputStream iconStream = getClass().getResourceAsStream("/icon.png");
+        Image image = new Image(iconStream);
+
         FXMLLoader loader = new FXMLLoader();
         URL xmlUrl = getClass().getResource("/main.fxml");
         loader.setLocation(xmlUrl);
         Parent root = loader.load();
 
         stage.setScene(new Scene(root));
-        stage.setWidth(500);
-        stage.setHeight(700);
+        stage.getIcons().add(image);
+        stage.setTitle("Calculator");
         stage.show();
     }
 
